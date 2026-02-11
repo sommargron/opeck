c65e7259bbebdf48678ba2d9407ebb723e3f6e36
sommargron <karlemritz@gmail.com>
2026-02-11 14:05:09 +0000

    table1b

--- 04_desc.R
+++ 04_desc.R
@@ -1,4 +1,4 @@
-# --- helpers -----------------------------------------------------------------
+# Helper functions -----------------------------------------------------------------
 
 fmt_mean_sd <- function(x) {
   x <- x[is.finite(x)]
@@ -19,7 +19,7 @@ fmt_n_pct <- function(x) {
   sprintf("%d (%.1f%%)", n, 100 * n / d)
 }
 
-# --- Table 1A: CKD-free at baseline, by vgdffm --------------------------------
+# Table 1A: CKD-free at baseline, by vgdffm --------------------------------
 table1a <- left_join(
   opeck_o2,
   opeck_e3,
@@ -65,3 +65,50 @@ table1a <- left_join(
   pivot_wider(names_from = rowname)
 
 writexl::write_xlsx(table1a, path = 'opeck/outputs/table1a.xlsx')
+
+# Table 1B: CKD at baseline, by vgdffm --------------------------------
+table1b <- left_join(
+  opeck_o2,
+  opeck_e3,
+  by = 'opeck_id'
+) %>% 
+  filter(n18 <= date_enrol,
+         is.na(n07)
+  ) %>%
+  mutate(
+    vgdffm = case_when(
+      is.na(vgdffm) ~ NA_integer_,
+      vgdffm == 0   ~ 0,
+      vgdffm <= 1   ~ 1,
+      vgdffm > 1    ~ 2),
+    uacr = if_else(!is.na(uacr), uacr >= 3, NA)
+  ) %>%
+  select(vgdffm, age, sex, yob, smo, bmi, alc, dep, eth, qua, inc, egfr, uacr) %>%
+  mutate(vgdffm = case_match(vgdffm, 
+                             0 ~ "Unexposed", 
+                             1 ~ "1-10 EU-years", 
+                             2 ~ ">10 EU-years")) %>%
+  group_by(vgdffm) %>%
+  summarise(
+    N = as.character(n()),
+    `Age at baseline, years, mean (IQR)` = fmt_mean_sd(age),
+    `Female, n (%)`     = fmt_n_pct(sex == 0),
+    `Year of birth, median (IQR)` = fmt_median_iqr(yob),
+    `Never-smokers, n (%) `         = fmt_n_pct(smo == 0),
+    `BMI, kg/m^2, mean (SD)`            = fmt_mean_sd(bmi),
+    `Alcohol >=1 per week, n (%)`         = fmt_n_pct(alc %in% c(1,2,3)),
+    `Townsend deprivation index, mean (SD)` = fmt_mean_sd(dep),
+    `Ethnicity, % White` = fmt_n_pct(eth == 1),
+    `Education, median (IQR)` = fmt_median_iqr(qua),
+    `Income <18 000 GBP, n (%)`    = fmt_n_pct(inc == 1),
+    `eGFR, mean (SD)` = fmt_mean_sd(egfr),
+    `eGFR < 60, n (%)` = fmt_n_pct(egfr < 60),
+    `uACR >= 3, n (%)` = fmt_n_pct(uacr),
+    .groups = "drop"
+  ) %>%
+  relocate(vgdffm) %>% 
+  rownames_to_column() %>% 
+  pivot_longer(-rowname) %>%
+  pivot_wider(names_from = rowname)
+
+writexl::write_xlsx(table1b, path = 'opeck/outputs/table1b.xlsx')
